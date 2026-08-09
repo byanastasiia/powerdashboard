bfc18aa5f513b64213e0edc3dbf9116ac2ba5d28
byanastasiia <anastasiia.by@gmail.com>
2026-08-09 18:26:46 +0300

    Update powerdashboard.py

--- powerdashboard.py
+++ powerdashboard.py
@@ -2152,4 +2152,4 @@ def render_well_panel(well, period):
 
 # ════════════════════════════════════════════════
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)
